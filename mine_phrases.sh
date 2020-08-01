053af73dedb2959f6ace863cd1bc6b7260ccd348
EC2 Default User <ec2-user@ip-172-31-88-53.ec2.internal>
2020-08-01 21:23:23 +0000

    working out the auto phrase kinks

--- mine_phrases.sh
+++ mine_phrases.sh
@@ -1,1 +1,3 @@
-(cd AutoPhrase && ./auto_phrase.sh)
+# optionall add RAW_LABEL_FILE=../data/labels.label
+
+(cd AutoPhrase && RAW_TRAIN=../data/reviews.txt ./auto_phrase.sh)
--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 
 sudo yum install -y \
-    git \
     gcc-c++ \
-    java-1.6.0-openjdk-devel
+    java-1.8.0-openjdk-devel
 
 [ ! -d "./AutoPhrase" ] && git clone https://github.com/shangjingbo1226/AutoPhrase.git
 
